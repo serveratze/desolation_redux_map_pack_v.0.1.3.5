2206c0317e8be21546d1c8963bd780bcd08a19ae
110 <schmiedebums@gmx.de>
2017-08-08 17:24:19 +0200

    Delete all_maps.sql

--- sql/all_maps.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `world` (`uuid`, `name`, `map`) VALUES
-(0x11e66ac33a4ccd1c82c510bf48883ace, 'My Tanoa Server', 'Tanoa'),
-(0x11e65ac32a4ccd1c82c511bf48983acf, 'My Altis Server', 'Altis'),
-(0x18e61ac33a8ccd1c54c510bf47892acb, 'My Stratis Server', 'Stratis'),
-(0x13e45ac38a4ccd1c82c511bf48783aca, 'My Malden Server', 'Malden'),
-(0x11e71379043c8906833008626681de3d, 'My Chernarus Server', 'Chernarus');
